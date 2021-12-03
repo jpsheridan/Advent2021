3179f77277043b9bd9e344ddf96f927994dcce50
jpsheridan <76212519+jpsheridan@users.noreply.github.com>
2021-12-03 07:07:49 +0000

    days2+3

--- Day02.cs
+++ Day02.cs
@@ -18,16 +18,30 @@ namespace AdventCode
 
             string ln = "";
             int valid = 0;
-
+            long x = 0;
+            long y = 0;
             while ((ln=sr.ReadLine())!=null)
             {
-
+                string[] move = ln.Split(' ');
+                long diff = int.Parse(move[1]);
+                switch (move[0])
+                {
+                    case "up":
+                        y -= diff;
+                        break;
+                    case "down":
+                        y += diff;
+                        break;
+                    case "forward":
+                        x += diff;
+                        break;
+                }
             }
             sw.Stop();
 
             sr.Close();
-
-            string ret = "Answer : " + valid.ToString();
+            long ans = x * y;
+            string ret = "Answer : " + ans.ToString();
             ret += Environment.NewLine + "Time: " + sw.ElapsedMilliseconds.ToString();
             return ret;
         }
@@ -40,20 +54,37 @@ namespace AdventCode
 
             string ln = "";
             int valid = 0;
+            long x = 0;
+            long y = 0;
+            long aim = 0;
 
             while ((ln = sr.ReadLine()) != null)
             {
-
+                string[] move = ln.Split(' ');
+                long diff = int.Parse(move[1]);
+                switch (move[0])
+                {
+                    case "up":
+                        aim -= diff;
+                        break;
+                    case "down":
+                        aim += diff;
+                        break;
+                    case "forward":
+                        x += diff;
+                        y += aim * diff;
+                        break;
+                }
             }
             sw.Stop();
 
             sr.Close();
-
-            string ret = "Answer : " + valid.ToString();
+            long ans = x * y;
+            string ret = "Answer : " + ans.ToString();
             ret += Environment.NewLine + "Time: " + sw.ElapsedMilliseconds.ToString();
             return ret;
 
-        
+
         }
     
 
--- Day03.cs
+++ Day03.cs
@@ -18,11 +18,38 @@ namespace AdventCode
 
             string ln = "";
             int valid = 0;
-
+            int[] c = new int[12];
+            int rows = 0;
             while ((ln = sr.ReadLine()) != null)
             {
-
+                rows++;
+                for (int i = 0; i < ln.Length; i++)
+                {
+                    c[i] += int.Parse(ln.Substring(i, 1));
+                }
             }
+
+            string eps = "";
+            string gam = "";
+            int max = rows / 2;
+            for (int i = 0; i<12; i++)
+            {
+                if (c[i]> max)
+                {
+                    gam += "1";
+                    eps += "0";
+                }
+                else
+                {
+                    gam += "0";
+                    eps += "1";
+                }
+            }
+
+            int g = Convert.ToInt32(gam, 2);
+            int e = Convert.ToInt32(eps, 2);
+
+            valid = g * e;
             sw.Stop();
 
             sr.Close();
@@ -38,13 +65,32 @@ namespace AdventCode
             sw.Start();
             StreamReader sr = new StreamReader("c:\\temp\\advent_2021\\advent_2021_" + this.GetType().Name + ".txt");
 
+
             string ln = "";
             int valid = 0;
-
+            int strlen = 12;
+            int[] c = new int[strlen];
+            int rows = 0;
+            List<string> diag = new List<string>();
             while ((ln = sr.ReadLine()) != null)
             {
+                rows++;
+                for (int i = 0; i < ln.Length; i++)
+                {
+                    c[i] += int.Parse(ln.Substring(i, 1));
+                }
+                diag.Add(ln);
 
             }
+
+            string gam = matchnums("gam", diag);
+            string eps = matchnums("eps", diag);
+
+            int g = Convert.ToInt32(gam, 2);
+            int e = Convert.ToInt32(eps, 2);
+
+            valid = g * e;
+        
             sw.Stop();
 
             sr.Close();
@@ -56,5 +102,59 @@ namespace AdventCode
 
         }
 
+        string matchnums (string match, List<string> nums)
+        {
+            List<string> matched = new List<string>(nums);
+            List<string> temp0 = new List<string>();
+            List<string> temp1 = new List<string>();
+
+            
+            for (int i = 0; i < nums[0].Length; i++)
+            {
+                int max = (int) Math.Ceiling((double)matched.Count / 2);
+
+                int ones = 0;
+                for (int j = 0; j < matched.Count; j++)
+                {
+                    if (matched[j].Substring(i,1)== "1")
+                    {
+                        temp1.Add(matched[j]);
+                        ones++;
+                    }
+                    else
+                    {
+                        temp0.Add(matched[j]);
+                    }
+                }
+                                
+                if (ones >= max)
+                {
+                    if (match == "gam")
+                        matched = new List<string>(temp1);
+                    else
+                        matched = new List<string>(temp0);
+                }
+                else
+                {
+                    if (match == "gam")
+                        matched = new List<string>(temp0);
+                    else
+                        matched = new List<string>(temp1);
+                    
+                }
+
+                temp0.Clear();
+                temp1.Clear();
+                ones = 0;
+
+                if (matched.Count==1)
+                {
+                    break;
+                }
+
+            }
+
+            return matched[0];
+        }
     }
 }
